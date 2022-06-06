64c8e80fce42554734becf1c15a2c0eea3b5cc54
mozammalrahat <hossain.rahat33@gmail.com>
2022-06-06 21:27:09 +0600

    Order Summary Added

--- supplier/index.js
+++ supplier/index.js
@@ -3,28 +3,77 @@ const mongoose = require('mongoose');
 require('dotenv').config({path:"./config.env"});
 const Product = require('./models/product');
 const connectDB = require('./connectDB');
+const axios = require('axios');
 connectDB();
 const app = express();
+app.use(express.json());
 port = 3002;
 app.get('/', (req, res) => {
     res.send('Hello World!');
 }
 );
+app.post('/order',async(req,res)=>{
 
-app.get('/create_product', async (req, res) => {
-    res.send('this is from the create_product endpoint');
-    const product = new Product({
-        name: 'Product 1',
-        price: 100,
-        description: 'Product 1 description',
-        available: true,
-        amount: 10,
-        image: 'https://www.pinterest.com/pin/687784174355786719/',
-        category: 'category 1',
+    order = req.body.order;
+    ecommerce_account = req.body.ecommerce_account;
+    ecommerce_secret = req.body.secret_key;
+
+    if(!order || !ecommerce_account || !ecommerce_secret){
+        res.status(400).send({
+            message: 'Invalid Request'
+        });
+    }
+    else{
+
+        for(let i=0;i<order.products.length;i++){
+            product = await Product.findOne({key:order.products[i].key});
+            if(product){
+                product.amount = product.amount - order.products[i].quantity;
+                await product.save();
+            }
+        }
+
+        const bank_transaction = await axios.post(
+            'http://localhost:3001/bank/create_transaction',
+            {
+                fromAccount: ecommerce_account,
+                bank_secret:ecommerce_secret,
+                toAccount: process.env.supplier_account,
+                amount: order.totalPrice
+            }
+        )
+        console.log("The Bank Transaction is",bank_transaction.data);
+
+        if(bank_transaction.data.success){
+            res.status(200).send({
+                message: 'Order Placed'
+            });
+        }
+        else{
+            res.status(400).send({
+                message: 'Order Failed'
+            });
+        }
+    }
+    
+})
+app.post('/create_product', async (req, res) => {
+    
+    const product = req.body;
+    console.log("product", req.body);
+    const newproduct = new Product({
+        name: product.name,
+        key: product.key,
+        price: product.price,
+        description: product.description,
+        available: product.available,
+        amount: product.amount,
+        image: product.image,
+        category: product.category,
         createdAt: new Date()
     });
 
-    await product.save((err, product) => {
+    await newproduct.save((err, product) => {
         if (err) {
             console.log(err);
         }
@@ -33,6 +82,13 @@ app.get('/create_product', async (req, res) => {
         }
     }
     );
+
+    res.status(200).json({ newproduct });
+});
+
+app.get('/products', async (req, res) => {
+    const products = await Product.find();
+    res.json(products);
 });
 
 
--- supplier/models/product.js
+++ supplier/models/product.js
@@ -3,6 +3,7 @@ const Schema    = mongoose.Schema;
 
 const productSchema = new Schema({
     name: String,
+    key: String,
     price: Number,
     description: String,
     available: Boolean,
